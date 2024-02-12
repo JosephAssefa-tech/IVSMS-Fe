e6cffd33465363da145f6ada98406f535ac8763d
JosephAssefa-tech <yosephassefa85@gmail.com>
2024-02-12 22:58:07 +0300

    working on factories ui

---
+++ src/components/Inflation/inflation/inflation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inflation',
+  templateUrl: './inflation.component.html',
+  styleUrl: './inflation.component.css'
+})
+export class InflationComponent {
+
+}
